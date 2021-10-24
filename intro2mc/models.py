7fa79893c1938a9e982109d26d27d1eb9be7dc25
harry8698 <harry8698@icloud.com>
2021-10-24 00:44:32 -0400

    add models

--- intro2mc/models.py
+++ intro2mc/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+######## Attendance ########
+class Student(models.Model):
+    andrewID = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=200)
+    IGN = models.CharField(max_length=200)
+
+class ClassSession(models.Model):
+    id = models.AutoField(primary_key=True)
+    term = models.CharField(max_length=200)
+    date = models.DateTimeField()
+
+class Attendance(models.Model):
+    andrewID = models.CharField(max_length=200, primary_key=True)
+    term = models.CharField(max_length=200)
+    classSession = models.ForeignKey(ClassSession, on_delete=models.PROTECT)
+    excused = models.BooleanField(default=False)
+
+######## Web app settings ########
+# Adapted from https://www.rootstrap.com/blog/simple-dynamic-settings-for-django/
+class Singleton(models.Model):
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(Singleton, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, _ = cls.objects.get_or_create(pk=1)
+        return obj
+        
+class AppConfig(Singleton):
+    id = models.AutoField(primary_key=True)
+    syllabus = models.CharField(max_length=200)
+    mapURL = models.CharField(max_length=200)
+    currSemester = models.CharField(max_length=200)
+    attendanceID = models.CharField(max_length=200)
+
+######## Website contents ########
+class Video(models.Model):
+    id = models.AutoField(primary_key=True)
+    videoURL = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+class Meme(models.Model):
+    id = models.AutoField(primary_key=True)
+    imageURL = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+class Resource(models.Model):
+    id = models.AutoField(primary_key=True)
+    URL = models.CharField(max_length=200)
+    imageURL = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
