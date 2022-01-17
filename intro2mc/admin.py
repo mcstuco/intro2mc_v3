c5b73ea1bf0c10f5b6ec2e122e811bda1fc9602b
Zhengze Gong <harry8698@icloud.com>
2022-01-16 23:29:21 -0500

    add models to admin page;

--- intro2mc/admin.py
+++ intro2mc/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from intro2mc.models import *
 
 # Register your models here.
+class AttendanceAdmin(admin.ModelAdmin):
+    fields = [field.name for field in Attendance._meta.concrete_fields]
+    readonly_fields = ['id', 'created_at', 'updated_at']
+    class Meta:
+        model = Attendance
+
+class ClassSessionAdmin(admin.ModelAdmin):
+    fields = [field.name for field in ClassSession._meta.concrete_fields]
+    readonly_fields = ['created_at', 'updated_at']
+    class Meta:
+        model = ClassSession
+
+class MemeAdmin(admin.ModelAdmin):
+    fields = [field.name for field in Meme._meta.concrete_fields]
+    readonly_fields = ['created_at', 'updated_at']
+    class Meta:
+        model = Meme
+
+class ResourceAdmin(admin.ModelAdmin):
+    fields = [field.name for field in Resource._meta.concrete_fields]
+    readonly_fields = ['created_at', 'updated_at']
+    class Meta:
+        model = Resource
+
+class StudentAdmin(admin.ModelAdmin):
+    fields = [field.name for field in Student._meta.concrete_fields]
+    readonly_fields = ['created_at', 'updated_at']
+    class Meta:
+        model = Student
+
+class VideoAdmin(admin.ModelAdmin):
+    fields = [field.name for field in Video._meta.concrete_fields]
+    readonly_fields = ['created_at', 'updated_at']
+    class Meta:
+        model = Video
+
+admin.site.register(AppConfig)
+admin.site.register(Attendance, AttendanceAdmin)
+admin.site.register(ClassSession, ClassSessionAdmin)
+admin.site.register(Meme, MemeAdmin)
+admin.site.register(Resource, ResourceAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Video, VideoAdmin)
--- intro2mc/models.py
+++ intro2mc/models.py
@@ -1,23 +1,5 @@
 from django.db import models
 
-######## Attendance ########
-class Student(models.Model):
-    andrewID = models.CharField(max_length=200, primary_key=True)
-    name = models.CharField(max_length=200, default='')
-    picture = models.CharField(max_length=200)
-    IGN = models.CharField(max_length=200)
-
-class ClassSession(models.Model):
-    id = models.AutoField(primary_key=True)
-    term = models.CharField(max_length=200)
-    date = models.DateTimeField()
-
-class Attendance(models.Model):
-    andrewID = models.CharField(max_length=200, primary_key=True)
-    term = models.CharField(max_length=200)
-    classSession = models.ForeignKey(ClassSession, on_delete=models.PROTECT)
-    excused = models.BooleanField(default=False)
-
 ######## Web app settings ########
 # Adapted from https://www.rootstrap.com/blog/simple-dynamic-settings-for-django/
 class Singleton(models.Model):
@@ -45,18 +27,42 @@ class AppConfig(Singleton):
     # All andrew ids on the roster, separated by ','
     roster = models.TextField(blank=True)
 
+class HasTimeStamps(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+######## Attendance ########
+class Student(HasTimeStamps):
+    andrewID = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=200, default='')
+    picture = models.CharField(max_length=200)
+    IGN = models.CharField(max_length=200)
+    uuid = models.CharField(max_length=200)
+
+class ClassSession(HasTimeStamps):
+    date = models.DateField(primary_key=True)
+    term = models.CharField(max_length=200)
+
+class Attendance(HasTimeStamps):
+    id = models.AutoField(primary_key=True)
+    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    term = models.CharField(max_length=200)
+    classSession = models.ForeignKey(ClassSession, on_delete=models.PROTECT)
+    excused = models.BooleanField(default=False)
+
 ######## Website contents ########
-class Video(models.Model):
-    id = models.AutoField(primary_key=True)
-    videoURL = models.CharField(max_length=200)
+class Video(HasTimeStamps):
+    videoURL = models.CharField(max_length=200, primary_key=True)
     description = models.TextField(blank=True)
 
-class Meme(models.Model):
-    id = models.AutoField(primary_key=True)
-    imageURL = models.CharField(max_length=200)
+class Meme(HasTimeStamps):
+    imageURL = models.CharField(max_length=200, primary_key=True)
     description = models.TextField(blank=True)
 
-class Resource(models.Model):
+class Resource(HasTimeStamps):
     id = models.AutoField(primary_key=True)
     URL = models.CharField(max_length=200)
     imageURL = models.CharField(max_length=200)
