d4cde4e055c3d8dd1e9c2c1bc36e6e15c0ea3de4
buhiroshi0205 <buhiroshi@hotmail.com>
2022-01-24 23:39:30 +0000

    added __str__ for assignment and submissions

--- intro2mc/models.py
+++ intro2mc/models.py
@@ -88,9 +88,15 @@ class Assignment(HasTimeStamps):
     userSubmittable = models.BooleanField(default=False)
     gradeReleased = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.term}-{self.name}'
+
 class Submission(HasTimeStamps):
     id = models.AutoField(primary_key=True)
     assignment = models.ForeignKey(Assignment, on_delete=models.PROTECT)
     student = models.ForeignKey(Student, on_delete=models.PROTECT)
     details = models.TextField(blank=True)
     grade = models.CharField(max_length=1, default='U')
+
+    def __str__(self):
+        return f'{self.assignment}-{self.student}'
