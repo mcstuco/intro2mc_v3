e242696d01fc8afc702b6c936f475538895ad30c
Hanke Chen <i@kokecacao.me>
2025-01-12 23:58:44 -0500

    :sparkles: update readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# intro2mc_v3
+
+You should look at `provision-mc.sh` and `setup.sh`
--- setup.sh
+++ setup.sh
@@ -5,11 +5,22 @@ sudo apt install git-lfs openjdk-17-jre-headless sqlite3 -y
 
 # install python django and dependencies
 sudo apt install python3-pip libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0 -y
-pip install --upgrade pip
-pip install -r ./requirements.txt
+sudo apt install libjpeg-dev zlib1g-dev -y # for pillow
+
+# If you use ubuntu >22.04, then you should create venv
+# Because: https://stackoverflow.com/questions/75602063/pip-install-r-requirements-txt-is-failing-this-environment-is-externally-mana
+python3 -m venv .venv
+source .venv/bin/activate
+
+python3 -m pip install setuptools
+pip3 install --upgrade pip # you may skip it if it generates warnings
+pip3 install -r ./requirements.txt # or python3 -m pip install -r requirements.txt if you use venv
 
 # install apache2 and configure wsgi
 sudo apt install apache2 libapache2-mod-wsgi-py3 -y
+
+# make sure your python version is correct in ./apache2.conf
+# also your installation path is correct for all project (search and replace /intro2mc_v3)
 cat ./apache2.conf | sudo tee /etc/apache2/apache2.conf > /dev/null
 
 sudo chgrp -R www-data ../intro2mc_v3/
@@ -24,16 +35,23 @@ sudo chmod -R g+w ../intro2mc_v3/
 # Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
 
 # setup django
-
 python3 manage.py runserver # should kill
-sudo chgrp -R www-data ../intro2mc_v3/
-sudo chmod -R g+w ../intro2mc_v3/
-# I don't know what's the better way to create the database
+sudo chgrp -R www-data /home/mcstuco/Documents/intro2mc_v3
+sudo chmod -R g+w /home/mcstuco/Documents/intro2mc_v3
 
+sudo chgrp www-data /home/mcstuco/Documents/
+sudo chmod g+w /home/mcstuco/Documents/
+sudo chgrp www-data /home/mcstuco/
+sudo chmod g+w /home/mcstuco/
+# you can't pass relative paths like ../intro2mc_v3/ as it doesn't work
+# sudo -u www-data test -r /home/mcstuco/Documents/intro2mc_v3/db.sqlite3; echo "$?" # should return 0
+# otherwise that means apache can't read the file
+
+# I don't know what's the better way to create the database
 python3 manage.py makemigrations intro2mc
 python3 manage.py migrate
-python3 manage.py createsuperuser
-python3 manage.py collectstatic # will prompt
+python3 manage.py createsuperuser # will prompt you to provide password
+python3 manage.py collectstatic
 
 # reload apache
 sudo service apache2 restart
@@ -66,3 +84,6 @@ sudo iptables -P FORWARD ACCEPT && \
 sudo iptables -F && \
 sudo iptables-save | sudo tee /etc/iptables.conf && \
 echo "My service will automatically do sudo iptables-restore < /etc/iptables.conf to load saved iptables.conf on server start."
+
+# make sure you set .env
+# and then look at provision-mc.sh for the rest of the steps (maybe, or maybe not)
